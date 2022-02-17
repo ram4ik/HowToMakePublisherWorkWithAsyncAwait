c71d81329141b10bb57fb49d47fb88ee3cabc2b4
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-02-17 14:04:14 +0200

    Initial Commit

---
+++ HowToMakePublisherWorkWithAsyncAwait/HowToMakePublisherWorkWithAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToMakePublisherWorkWithAsyncAwaitApp.swift
+//  HowToMakePublisherWorkWithAsyncAwait
+//
+//  Created by ramil on 17.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct HowToMakePublisherWorkWithAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
